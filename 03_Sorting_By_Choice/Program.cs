a226fae88784874f8c4a171883720d2ebad062dc
gregory <smartis2816@gmail.com>
2022-08-04 11:36:14 +0300

    03 comleted

--- 03_Sorting_By_Choice/Program.cs
+++ 03_Sorting_By_Choice/Program.cs
@@ -1,4 +1,55 @@
-﻿//
+﻿// Сортировка выбором
+
+// Заполнение массива
+Console.WriteLine("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Введите число: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine();
+Console.WriteLine("Начальный массив: [" + string.Join(", ", array) + "]");
+
+// Сортировка
+for (int i = 0; i < n - 1; i++)
+{
+    int minIndex = i;
+    for (int j = i + 1; j < n; j++)
+    {
+        if (array[j] < array[minIndex])
+            minIndex = j;
+    }
+    int temp;
+    temp = array[minIndex];
+    array[minIndex] = array[i];
+    array[i] = temp;
+}
+Console.WriteLine("Конечный массив: [" + string.Join(", ", array) + "]");
 
 
+// Сортировка массива строк
+/*
+string[] array = new string[5];
+for (int i = 0; i < 5; i++)
+{
+    array[i] = Console.ReadLine();
+}
 
+Console.Write("[" + string.Join(", ", array) + "]");
+for (int i = 0; i < 4; i++)
+{
+    int MinIndex = i;
+    for (int j = i + 1; j < 5; j++)
+    {
+        if (array[j].Length < array[MinIndex].Length)
+            MinIndex = j; 
+    }
+    string temp;
+    temp = array[MinIndex];
+    array[MinIndex] = array[i];
+    array[i] = temp;
+}
+Console.WriteLine("Конечный массив: [" + string.Join(", ", array) + "]");
+*/
